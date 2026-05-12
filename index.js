f18dd7455e08a7c7137e56eea06343112c33272d
Nimra Mittal <nimra.mittla.s.125@kalvium.community>
2026-05-12 21:01:31 +0530

    debug

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config()
 
 const app = express()
 
-app.arguments(express.json())
+app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("Connect to Mongoose"))
